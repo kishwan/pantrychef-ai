0f5ce57ab0918aa991ca65ac5eee941cbbe19bae
kishwan <kishanbmuk@gmail.com>
2025-08-18 20:17:33 -0400

    Remove public key

--- backend/clients/themealdb_client.py
+++ backend/clients/themealdb_client.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-THEMEALDB_API_KEY = os.getenv("THEMEALDB_API_KEY", "1")
+THEMEALDB_API_KEY = os.getenv("THEMEALDB_API_KEY")
 BASE_URL = f"https://www.themealdb.com/api/json/v1/{THEMEALDB_API_KEY}"
 
 async def search_recipes_by_ingredients(ingredients: List[str]) -> Optional[List[Dict]]:
